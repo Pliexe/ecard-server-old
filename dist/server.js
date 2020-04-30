74760ad1eb380b67f99f9572cc5c85144123c4a4
Pliexe <42877326+Pliexe@users.noreply.github.com>
2020-04-30 10:11:08 +0200

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -1,14 +1,4 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+
 const SocketIO_1 = require("./SocketIO");
 const constants_1 = require("./constants");
 const user_1 = require("./Classes/user");
