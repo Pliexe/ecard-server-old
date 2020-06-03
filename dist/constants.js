b721aef59dd068747ee7faefd2893fac68950217
Pliexe <42877326+Pliexe@users.noreply.github.com>
2020-06-03 10:56:49 +0200

    a

--- dist/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SocketEvent;
-(function (SocketEvent) {
-    SocketEvent["CONNECT"] = "connection";
-    SocketEvent["DISCONNECT"] = "disconnect";
-})(SocketEvent = exports.SocketEvent || (exports.SocketEvent = {}));
-//# sourceMappingURL=constants.js.map
